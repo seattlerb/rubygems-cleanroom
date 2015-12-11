cc225c3e87a6507cc1684ae19398bff0224d095a
Ryan Davis <ryand@zenspider.com>
2015-12-10 16:06:06 -0800

    - Removed warning from redefining default_specifications_dir. [git-p4: depot-paths = "//src/rubygems-cleanroom/dev/": change = 10444]

--- lib/rubygems/cleanroom.rb
+++ lib/rubygems/cleanroom.rb
@@ -9,7 +9,13 @@ module Rubygems
   end
 end
 
+require "rubygems"
 class Gem::BasicSpecification # :nodoc:
+  if respond_to? :default_specifications_dir then
+    mc = class << self; self; end
+    mc.send :alias_method, :old_defspecdir, :default_specifications_dir
+  end
+
   def self.default_specifications_dir # :nodoc:
     ""
   end
